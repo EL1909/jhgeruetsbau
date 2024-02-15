916d3efb052cf064843bfcaf94d6606e479ba6fb
el1909 <efrain19091@gmail.com>
2024-02-15 11:04:28 +0100

    Account functionallties and alerts working as expected from modal

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,15 @@
 from allauth.account.forms import LoginForm, SignupForm
 from allauth.account.views import LoginView, SignupView
-from django.contrib.auth import authenticate, login, logout, get_user_model
-from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
 
 class LoginView(LoginView):
     template_name = 'accounts/login.html'
+    form_class = LoginForm
 
     def get(self, request):
         # If user is already authenticated, redirect to profile
@@ -17,54 +19,37 @@ class LoginView(LoginView):
 
     def post(self, request):
         # Process the login form data
+        form = self.form_class(request.POST, request.FILES)
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            # Redirect to home or some other page after successful login
-            return redirect('home')
+            # Return success response
+            return JsonResponse({'success': True})
         else:
-            # If authentication fails, render the login page again with error message
-            return render(request, self.template_name, {'error_message': 'Invalid username or password'})
-
+            # Return error response
+            errors = form.errors.as_json()
+            return JsonResponse({'errors':errors}, status=400)
 
 
 class SignupView(SignupView):
     template_name = 'account/signup.html'
+    form_class = SignupForm
 
     def get(self, request, *args, **kwargs):
-        form = SignupForm()
+        form = self.form_class()
         return render(request, self.template_name, {'form': form})
-    
-    def post(self, request, *args, **kwargs):
-        form = SignupForm(request.POST, request.FILES)
-        if form.is_valid():
-            user_model = get_user_model()
-            new_user = user_model.objects.create_user(
-                username=form.cleaned_data['username'],
-                email=form.cleaned_data['email'],
-                password=form.cleaned_data['password1'],
-            )
-            messages.success(request, 'Willkommen {{self.username}}')
-            return redirect(reverse('home'))
-        
-        return render(request, self.template_name, {'form': form})
-    
-    def clean(self):
-        cleaned_data = super().clean()
-        password1 = cleaned_data.get("password1")
-        password2 = cleaned_data.get("password2")
 
-        if password1 and password2 and password1 != password2:
-            messages.error('password2', 'Kennwort Bestätig')
-            
-        ## Validate Password using Django's built-in password validation
-        try:
-            password_validation.validate_password(password1, self.instance)
-        except ValidationError as e:
-            self.add_error('password1', e.messages[0])
-        return cleaned_data
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save(request=request)
+            messages.success(request, f'Welcome {user.username}! Your account has been successfully created.')
+            return JsonResponse({'success': True})
+        else:
+            errors = form.errors.as_json()
+            return JsonResponse({'errors':errors}, status=400)
 
 
 def direct_logout_view(request):
